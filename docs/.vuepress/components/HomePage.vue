6bf72054319376023407a9ba1e5d7c507bf3ae2e
Yi Liu <mail@imliuyi.com>
2021-12-04 19:46:40 +0800

    update

--- docs/.vuepress/components/HomePage.vue
+++ docs/.vuepress/components/HomePage.vue
@@ -1,134 +1,137 @@
 <template>
-  <n-grid x-gap="12" :cols="1" style="padding-top: 20%">
-    <n-gi>
-      <div style="text-align: center;">
-        <n-image id="logo" preview-disabled=true
-          style="width: 32%; border-radius: 50%; "
-          @mouseenter="hover_avatar = true"
-          @mouseleave="hover_avatar = false"
-          :src="
-            hover_avatar ? 'logo1.jpg' : (here ? '/logo2.jpg' : '/logo.jpg')
-          "
-        />
-      </div>
-    </n-gi>
-    <n-gi>
-      <div style="text-align: center">
-        <span
-          @mouseenter="hover_name = true"
-          @mouseleave="hover_name = false"
-          style="font-size: 2em"
-          >{{ hover_name ? "刘 逸" : "Yi Liu" }}</span
-        >
-      </div>
-      <div style="text-align: center"><span></span><span id="info"></span></div>
-    </n-gi>
-    <n-gi>
-      <div style="text-align: center; margin-top: 2%">
-        <n-popover
-          width="240px"
-          placement="bottom"
-          trigger="hover"
-          @update:show="handleUpdateShow"
-        >
-          <template #trigger>
-            <n-button round size="large" style="width: 300px;">
-              <span style="font-size: 18px">{{
-                here ? "Want to know or contact me?" : "Here!"
-              }}</span>
-            </n-button>
-          </template>
-          <template #header>
-            <n-text strong depth="1">
-              <div style="text-align: center">
-                <n-button
-                  text
-                  icon-placement="right"
-                  style="font-size: 20px"
-                  @click="successCopyEmail"
-                >
-                  <template #icon>
-                    <n-icon>
-                      <CopyOutlined />
-                    </n-icon>
-                  </template>
-                  mail@imliuyi.com
-                </n-button>
-                
-              </div>
-              
-            </n-text>
-          </template>
-          <n-grid x-gap="12" :cols="3" style="margin-top: 10px">
-            <n-gi>
-              <n-tooltip placement="bottom" trigger="hover">
-                <template #trigger>
-                  <div style="padding-bottom: 40px; padding-left: 7px">
-                    <a href="/YiLIU-CV-en.pdf" target="_blank">
-                      <n-button text>
-                        <template #icon>
-                          <n-icon size="50">
-                            <IdcardOutlined />
-                          </n-icon>
-                        </template>
-                      </n-button>
-                    </a>
-                  </div>
-                </template>
-                <div style="text-align: center; font-size: 20px">Resume</div>
-                <div style="text-align: center; font-size: 16px">
-                  <a href="/YiLIU-CV-zh_CN.pdf" target="_blank">[中文版]</a>
+  <div class="main">
+    <n-grid x-gap="12" :cols="1" style="padding-top: calc(22vh)">
+      <n-gi>
+        <div style="text-align: center">
+          <n-image
+            id="logo"
+            preview-disabled="true"
+            style="border-radius: 50%; width: calc(30vh)"
+            @mouseenter="hover_avatar = true"
+            @mouseleave="hover_avatar = false"
+            :src="hover_avatar ? 'logo1.jpg' : here_logo"
+          />
+        </div>
+      </n-gi>
+      <n-gi>
+        <div style="text-align: center">
+          <span
+            @mouseenter="hover_name = true"
+            @mouseleave="hover_name = false"
+            style="font-size: 2em"
+            >{{ hover_name ? "刘 逸" : "Yi Liu" }}</span
+          >
+        </div>
+        <div style="text-align: center">
+          <span></span><span id="info"></span>
+        </div>
+      </n-gi>
+      <n-gi>
+        <div style="text-align: center; margin-top: 2%">
+          <n-popover
+            width="240px"
+            placement="bottom"
+            trigger="hover"
+            @update:show="handleUpdateShow"
+          >
+            <template #trigger>
+              <n-button round size="large" style="width: 300px">
+                <span style="font-size: 18px">{{
+                  here ? "Want to know or contact me?" : "Here!"
+                }}</span>
+              </n-button>
+            </template>
+            <template #header>
+              <n-text strong depth="1">
+                <div style="text-align: center">
+                  <n-button
+                    text
+                    icon-placement="right"
+                    style="font-size: 20px"
+                    @click="successCopyEmail"
+                  >
+                    <template #icon>
+                      <n-icon>
+                        <CopyOutlined />
+                      </n-icon>
+                    </template>
+                    mail@imliuyi.com
+                  </n-button>
                 </div>
-              </n-tooltip>
-            </n-gi>
-            <n-gi>
-              <n-tooltip placement="bottom" trigger="hover">
-                <template #trigger>
-                  <div style="padding-bottom: 40px; padding-left: 7px">
-                    <a href="https://github.com/liu-yi" target="_blank">
-                      <n-button text>
-                        <template #icon>
-                          <n-icon size="50">
-                            <GithubFilled />
-                          </n-icon>
-                        </template>
-                      </n-button>
-                    </a>
+              </n-text>
+            </template>
+            <n-grid x-gap="12" :cols="3" style="margin-top: 10px">
+              <n-gi>
+                <n-tooltip placement="bottom" trigger="hover">
+                  <template #trigger>
+                    <div style="padding-bottom: 40px; padding-left: 7px">
+                      <a href="/YiLIU-CV-en.pdf" target="_blank">
+                        <n-button text>
+                          <template #icon>
+                            <n-icon size="50">
+                              <IdcardOutlined />
+                            </n-icon>
+                          </template>
+                        </n-button>
+                      </a>
+                    </div>
+                  </template>
+                  <div style="text-align: center; font-size: 20px">Resume</div>
+                  <div style="text-align: center; font-size: 16px">
+                    <a href="/YiLIU-CV-zh_CN.pdf" target="_blank">[中文版]</a>
                   </div>
-                </template>
-                <div style="text-align: center; font-size: 20px">Github</div>
-              </n-tooltip>
-            </n-gi>
-            <n-gi>
-              <n-tooltip placement="bottom" trigger="hover">
-                <template #trigger>
-                  <div style="padding-bottom: 40px; padding-left: 7px">
-                    <a
-                      href="https://www.linkedin.com/in/%E9%80%B8-%E5%88%98-363764114/"
-                      target="_blank"
-                    >
-                      <n-button text>
-                        <template #icon>
-                          <n-icon size="50">
-                            <LinkedinFilled />
-                          </n-icon>
-                        </template>
-                      </n-button>
-                    </a>
+                </n-tooltip>
+              </n-gi>
+              <n-gi>
+                <n-tooltip placement="bottom" trigger="hover">
+                  <template #trigger>
+                    <div style="padding-bottom: 40px; padding-left: 7px">
+                      <a href="https://github.com/liu-yi" target="_blank">
+                        <n-button text>
+                          <template #icon>
+                            <n-icon size="50">
+                              <GithubFilled />
+                            </n-icon>
+                          </template>
+                        </n-button>
+                      </a>
+                    </div>
+                  </template>
+                  <div style="text-align: center; font-size: 20px">Github</div>
+                </n-tooltip>
+              </n-gi>
+              <n-gi>
+                <n-tooltip placement="bottom" trigger="hover">
+                  <template #trigger>
+                    <div style="padding-bottom: 40px; padding-left: 7px">
+                      <a
+                        href="https://www.linkedin.com/in/%E9%80%B8-%E5%88%98-363764114/"
+                        target="_blank"
+                      >
+                        <n-button text>
+                          <template #icon>
+                            <n-icon size="50">
+                              <LinkedinFilled />
+                            </n-icon>
+                          </template>
+                        </n-button>
+                      </a>
+                    </div>
+                  </template>
+                  <div style="text-align: center; font-size: 20px">
+                    LinkedIn
                   </div>
-                </template>
-                <div style="text-align: center; font-size: 20px">LinkedIn</div>
-              </n-tooltip>
-            </n-gi>
-          </n-grid>
-          <!-- <n-divider />
+                </n-tooltip>
+              </n-gi>
+            </n-grid>
+            <!-- <n-divider />
           <div style="font-size: 16px; text-align: center">
             <a href="/papers/">See my papers</a>
           </div> -->
-        </n-popover>
-      </div>
-    </n-gi>
-    <!-- <n-gi>
+          </n-popover>
+        </div>
+      </n-gi>
+      <!-- <n-gi>
       <n-button @click="show_paper = !show_paper">点击来转圈</n-button>
       <n-space vertical>
         <n-spin :show="show_paper">
@@ -139,7 +142,8 @@
         </n-spin>
       </n-space>
     </n-gi> -->
-  </n-grid>
+    </n-grid>
+  </div>
 </template>
 
 <script>
@@ -152,8 +156,8 @@ import {
 } from "@vicons/antd";
 import { defineComponent, ref, getCurrentInstance } from "vue";
 import { init } from "ityped";
-import { useMessage } from 'naive-ui';
-import { copyText } from 'vue3-clipboard'
+import { useMessage } from "naive-ui";
+import { copyText } from "vue3-clipboard";
 
 export default defineComponent({
   data: function () {
@@ -163,21 +167,28 @@ export default defineComponent({
       here: false,
     };
   },
+  computed: {
+    here_logo() {
+      return this.here ? "/logo2.jpg" : "/logo.jpg";
+    },
+  },
   setup() {
     const o = getCurrentInstance();
     const email = ref("mail@imliuyi.com");
     const message = useMessage();
     return {
       successCopyEmail() {
-        copyText('mail@imliuyi.com', undefined, (error, event) => {
+        copyText("mail@imliuyi.com", undefined, (error, event) => {
           if (error) {
             message.warning("Can not copy. ", { duration: 3000 });
-            console.log(error)
+            console.log(error);
           } else {
-            message.success("The e-mail address is copied. ", { duration: 3000 });
-            console.log(event)
+            message.success("The e-mail address is copied. ", {
+              duration: 3000,
+            });
+            console.log(event);
           }
-        })
+        });
       },
       show_paper: ref(false),
       showPopover: ref(false),
@@ -212,16 +223,11 @@ export default defineComponent({
 </script>
 
 <style>
-.n-card {
-  margin-top: 20%;
-  max-width: 100%;
-}
-.light-green {
-  height: 108px;
-  background-color: rgba(0, 128, 0, 0.12);
-}
-.green {
-  height: 108px;
-  background-color: rgba(0, 128, 0, 0.24);
+html,
+body,
+.main {
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
 </style>
